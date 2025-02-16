7e4b311cf41e547d95e969f76755c4838c5c5757
Hakob <hakobsargsyan1993@gmail.com>
2025-02-16 22:58:36 +0400

    all

--- packages/utils/utils.test.js
+++ packages/utils/utils.test.js
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { greet } from "../index.js";
+import { greet } from "./index";
 
 test("greet function", () => {
     expect(greet("World")).toBe("Hello, World!");
